9e750e4866d6410ac2cbf89348d3956998b07dfb
Walter Bryan <wallythewebdev@gmail.com>
2020-02-05 18:09:06 +0000

    Update

---
+++ quote.js
@@ -0,0 +1,4 @@
+// const quotesComp = Vue.component('quote', {
+//     props: ['quote_String', 'author_String', 'date_string'],
+//     template: '<div class="quoteContainer"><p class="quote_String">{{quote_string}}</p><h3 class="author_String">{{aurthor_string}}</h3><p class="date_String">{{date_string}}</p></div>'
+// })
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Script has laoded')
+Vue.component('goal_template', {
+    props: ['goal_title', 'goal_description', 'goal_dateSet'],
+    
+    
+    template: '<div class="goal_container"><h3>{{goal_title}}</h3><p>{{goal_description}}</p><p>{{goal_dateSet}}</p></div>'
+    })
+
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        list: [
+        ],
+
+        // blank list for the list input to be added
+        listItem: {
+            title: '',
+            des: '',
+            date: null
+        }
+    }, 
+
+})
